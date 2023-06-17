d3335f91e6d8161414f330ec1dad46dc00ccb54b
Its-SheldonDev <marius.bzcn@gmail.com>
2023-06-17 14:00:29 +0200

    Edit isopen and css

--- js/isopen.js
+++ js/isopen.js
@@ -1,15 +1,9 @@
 const date = new Date();
 let hour = date.getHours();
 if (hour < 19 && hour > 5) {
-    document.getElementById('open').id = "close"; 
+    document.getElementById('open').id = "close";
+    document.getElementById('close').innerHTML = "<h1>CLOSE</h1><span><p>Ouverture de 19h30 à 05h00 du matin</p></span>";
+} else {
+    document.getElementById('open').id = "open";
+    document.getElementById('open').innerHTML = "<h1>OPEN</h1><span><p>Ouvert jusqu'à 05h00 du matin</p></span>";
 }
-else {
-    document.getElementById('open').id = "open"
-}
-if (document.getElementById('close')){
-    document.getElementById('close').innerHTML = "<h1>CLOSE</h1>"
-}
-else if (document.getElementById('open')){
-    document.getElementById('open').innerHTML = "<h1>OPEN</h1>"
-}
-
